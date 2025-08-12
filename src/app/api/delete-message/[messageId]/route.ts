c9d7d901ca17165fa16d2d51a27aaed1c5b2c276
harshiill <harshilkhandelwal28@gmail.com>
2025-08-12 11:10:40 +0530

    feat: add alert dialog component and implement message deletion functionality

---
+++ src/app/api/delete-message/[messageId]/route.ts
@@ -0,0 +1,47 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/models/user.model";
+import { NextRequest,NextResponse } from "next/server";
+import { authOptions } from "../../auth/[...nextauth]/options";
+import { getServerSession } from "next-auth";
+import mongoose from "mongoose";
+
+
+export async function DELETE(req : NextRequest,{params} : {params: {messageId: string}})
+{
+    const messageId = params.messageId;
+    await dbConnect();
+    const session = await getServerSession(authOptions);
+    const user = session?.user;
+
+    if(!session || !user) {
+        return NextResponse.json({
+            success: false,
+            message: "You must be signed in to get messages."
+        }, {status: 401});
+    }
+
+    try {
+       const updateResult = await UserModel.updateOne(
+            {_id: user._id},
+            {$pull: {messages: {_id: messageId}}}
+        )
+
+        if(updateResult.modifiedCount === 0) {
+            return NextResponse.json({
+                success: false,
+                message: "Failed to delete message."
+            }, {status: 500});
+        }
+
+        return NextResponse.json({
+            success: true,
+            message: "Message deleted successfully."
+        });
+    } catch (error) {
+        
+    }
+
+
+  
+}
--- src/components/MeesageCard.tsx
+++ src/components/MeesageCard.tsx
@@ -1,33 +1,88 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 'use client'
 import React from 'react'
+import { X } from 'lucide-react';
+import dayjs from 'dayjs';
+
 import {
   Card,
-  CardAction,
   CardContent,
-  CardDescription,
-  CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
+import { Button } from './ui/button'
+import { Message } from '@/models/user.model';
+import { toast } from 'sonner';
+import axios, { AxiosError } from 'axios';
+import { ApiResponse } from '@/types/ApiResponse';
 
-function MeesageCard() {
+
+type MessageCardProps={
+    message: Message;
+    onMessageDelete : (messageId : string) => void;
+}
+function MessageCard({message,onMessageDelete}: MessageCardProps) {
+    const handleDeleteConfirm = async () => {
+       try {
+        const res=await axios.delete<ApiResponse>(`/api/delete-message/${message._id}`)
+        toast.success(res.data.message)
+     
+     if (typeof message._id === "string") {
+       onMessageDelete(message._id);
+     } else {
+       toast.error("Invalid message id");
+     }
+       } catch (error) {
+const axiosError = error as AxiosError<ApiResponse>;
+toast.error(axiosError.response?.data.message || "Failed to delete message")
+
+       }
+
+    }
+
+
   return (
     <>
     <Card>
   <CardHeader>
-    <CardTitle>Card Title</CardTitle>
-    <CardDescription>Card Description</CardDescription>
-    <CardAction>Card Action</CardAction>
+    <div className="flex justify-between items-center"></div>
+    <CardTitle>{message.content}</CardTitle>
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <Button variant="destructive"><X className="w-5 h-5" /></Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+          <AlertDialogDescription>
+            This action cannot be undone. This will permanently delete
+                  this message.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction onClick={handleDeleteConfirm}>Continue</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+    
   </CardHeader>
   <CardContent>
-    <p>Card Content</p>
   </CardContent>
-  <CardFooter>
-    <p>Card Footer</p>
-  </CardFooter>
 </Card>
     </>
   )
 }
 
-export default MeesageCard
+export default MessageCard;
